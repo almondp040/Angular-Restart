9872c484e19fc3f28fb75e51e0089a0269a890e8
Almond Paschal <almondpaschal@gmail.com>
2025-11-03 18:20:32 -0500

    Directives Chapter Start!

--- Chapter 4 Directives.md
+++ Chapter 4 Directives.md
@@ -5,4 +5,4 @@
     - Used ONLY in a template, and some are built into Angular
     - We can also build our own as well! 
 
-    
+- These are the items we can use with @! 
---
+++ directives/src/app/app.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+ 
+ //Data: 
+  images = [
+
+    {
+      title: "At the beach", 
+      url: "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJlYWNoJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500"
+    }, 
+
+    {
+      title: "Palm Trees", 
+      url: "https://images.unsplash.com/photo-1544536871-6e891baa163f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687"
+    }, 
+
+    {
+      title: "Open Beach with Dock", 
+      url: "https://images.unsplash.com/photo-1553578827-0df58e305953?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYWNoJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500"
+    }, 
+
+    {
+      title: "Beach Sunset", 
+      url: "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYWNoJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500"
+    }, 
+
+    {
+      title: "Beach Water", 
+      url: "https://images.unsplash.com/photo-1500375592092-40eb2168fd21?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGJlYWNoJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500"
+    }
+
+  ]
+
+  
+}
